8f1cec4f3fc706a73b50853d9682022f1a729fac
kmietotwy <kmietotwy@gmail.com>
2019-11-26 11:02:00 -0800

    notes on JSX in app.js

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => <div>Start Here!</div>;
+
+export default App;
+
+// 1. JSX is syntactic sugar for React.createElement
+// 2. JSX can selfclose.
+// 3. User defined components should start with a capital letter
+// 4. You can use JavaScript expressions inside of JSX
+// 5. You can nest components just like you can in HTML
+// 6. Nested content can be accessed in props.children
