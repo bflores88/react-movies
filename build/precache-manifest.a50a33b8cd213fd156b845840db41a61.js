57b6dc89aa604140bb54387c13871b3414ad232a
kmietotwy <kmietotwy@gmail.com>
2019-11-27 11:36:06 -0800

    build folder and readme updated

---
+++ build/precache-manifest.a50a33b8cd213fd156b845840db41a61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8132cf968ec0de88d5aa5e16e936bf54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "876cd2635a7a7ba70510",
+    "url": "/static/js/2.a5e6049a.chunk.js"
+  },
+  {
+    "revision": "1e371cb4af8b0ab48ce6",
+    "url": "/static/js/main.0248c8d1.chunk.js"
+  },
+  {
+    "revision": "e75f9625cf1bd549ca78",
+    "url": "/static/js/runtime-main.6c55b726.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/static/media/reactMovie_logo.08494abf.png"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
