78523cb079fdd972023821cdb7363ee72e67780a
kmietotwy <kmietotwy@gmail.com>
2019-11-27 10:37:20 -0800

    mvp

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,38 @@
 import React from "react";
 
+// Components
+import Navigation from "./elements/Navigation";
+import MovieInfo from "./elements/MovieInfo";
+import MovieInfoBar from "./elements/MovieInfoBar";
+import Actor from "./elements/Actor";
+import Grid from "./elements/Grid";
+import Spinner from "./elements/Spinner";
+
+import { useMovieFetch } from "./hooks/useMovieFetch";
+
 const Movie = ({ movieId }) => {
-  return <div> Movie: {movieId} </div>;
+  const [movie, loading, error] = useMovieFetch(movieId);
+  console.log(movie);
+
+  if (error) return <div>Something went wrong ...</div>;
+  if (loading) return <Spinner />;
+
+  return (
+    <>
+      <Navigation movie={movie.original_title} />
+      <MovieInfo movie={movie} />
+      <MovieInfoBar
+        time={movie.runtime}
+        budget={movie.budget}
+        revenue={movie.revenue}
+      />
+      <Grid header="Actors">
+        {movie.actors.map(actor => (
+          <Actor key={actor.cast_id} actor={actor} />
+        ))}
+      </Grid>
+    </>
+  );
 };
 
 export default Movie;
--- src/components/elements/MovieInfoBar.js
+++ src/components/elements/MovieInfoBar.js
@@ -1,7 +1,34 @@
 import React from "react";
+import FontAwesome from "react-fontawesome";
+import { calcTime, convertMoney } from "../../helpers";
+import { StyledMovieInfoBar } from "../styles/StyledMovieInfoBar";
 
-const MovieInfoBar = () => {
-  return <div> Movie Info Bar</div>;
-};
+const MovieInfoBar = ({ time, budget, revenue }) => (
+  <StyledMovieInfoBar>
+    {" "}
+    <div className="movieinfobar-content">
+      <div className="movieinfobar-content-col">
+        <FontAwesome className="fa-time" name="clock-0" size="2x" />
+        <span className="movieinfobar-info">
+          Running time: {calcTime(time)}
+        </span>
+      </div>
+
+      <div className="movieinfobar-content-col">
+        <FontAwesome className="fa-budget" name="money" size="2x" />
+        <span className="movieinfobar-info">
+          Budget: {convertMoney(budget)}
+        </span>
+      </div>
+
+      <div className="movieinfobar-content-col">
+        <FontAwesome className="fa-revenue" name="ticket" size="2x" />
+        <span className="movieinfobar-info">
+          Revenue: {convertMoney(revenue)}
+        </span>
+      </div>
+    </div>
+  </StyledMovieInfoBar>
+);
 
 export default MovieInfoBar;
